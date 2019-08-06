a1e948b0a13eed3b8340f4babc252fcc0e1bc9d1
Yehan Pemarathne <yehancha@gmail.com>
2019-08-06 21:06:48 +0530

    Add Boostrap styling

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -22,8 +22,8 @@ const GuessedWords = props => {
       <div data-test='guessed-words'>
         <h3>Guessed Words</h3>
 
-        <table>
-          <thead>
+        <table className='table table-sm'>
+          <thead className='thead-light'>
             <tr>
               <th>Guess</th>
               <th>Matching Letters</th>
