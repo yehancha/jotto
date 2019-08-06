f1a5ea1b9048c981456f4d73575e6bc512416088
Yehan Pemarathne <yehancha@gmail.com>
2019-08-06 21:23:01 +0530

    Setup Redux

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,7 @@
+/**
+ * @function successReducer
+ * @param {array} state Array of guessed words.
+ * @param {object} action Action to be reduced.
+ * @returns {boolean} new success state.
+ */
+export default (state, action) => null;
